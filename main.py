06524031c00bb9e9d0f4c77831a61771b2d482bb
Saccharine-Coal <69489271+Saccharine-Coal@users.noreply.github.com>
2020-08-11 17:08:44 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from settings import *
 from sprites import *
 from generators import *
 from camera import *
+from converter import *
 
 
 class Game:
@@ -19,32 +20,15 @@ class Game:
         self.clock = pg.time.Clock()
         pg.key.set_repeat(500, 100)
         self.map = Generate()
+        self.iso = Converter()
 
     def new(self):
         """Initialize all variables and do all the setup for a new game."""
         self.all_sprites = pg.sprite.Group()
         self.tiles = pg.sprite.Group()
         self.map_background_data = self.map.generate_map_data_blank()
-        self._set_grid(self.map_background_data)
-        self.map_foreground_data = self.map.generate_map_data()
-        self._set_grid(self.map_foreground_data)
-        self.player = Player(self, 10, 10)
+        self.player = Player(self, 2, 2)
         self.camera = Camera(self.map.width, self.map.height)
-
-    def _set_grid(self, map_data):
-        for row_nb, row in enumerate(map_data):
-            for col_nb, tile in enumerate(row):
-                if tile == 0:
-                    tile_data = 'pixel dirt square'
-                if tile == 1:
-                    tile_data = 'overlay seed' #pixel square grass
-                if tile == 2:
-                    tile_data = 'overlay crop1' #pixel square flower
-                if tile == 3:
-                    tile_data = 'overlay crop2' #pixel square tree
-                if tile == 4:
-                    tile_data = 'overlay dirt'
-                Tile(self, tile_data, row_nb, col_nb)
 
     def run(self):
         """Game loop."""
@@ -68,17 +52,31 @@ class Game:
         self.all_sprites.update()
         self.camera.update(self.player)
 
-    def _update_foreground(self, x, y):
-        """Will update the tile data for the background."""
-        self.map_foreground_data[x][y] = 4
-        self._set_grid(self.map_foreground_data)
-
     def draw(self):
         """Draw the screen."""
         self.screen.fill(BGCOLOR)
+        self._draw_tiles(self.map_background_data)
+        for tile in self.tiles:
+            self.screen.blit(tile.image, self.camera.apply(tile))
         for sprite in self.all_sprites:
             self.screen.blit(sprite.image, self.camera.apply(sprite))
         pg.display.flip()
+
+    def _draw_tiles(self, map_data):
+        """Draws individual tiles, this creates a new object for each tile."""
+        for row_nb, row in enumerate(map_data):
+            for col_nb, tile in enumerate(row):
+                if tile == 0:
+                    tile_data = '128x128 green tile'
+                if tile == 1:
+                    tile_data = 'overlay seed tile' #pixel square grass
+                if tile == 2:
+                    tile_data = 'overlay flower1 tile' #pixel square flower
+                if tile == 3:
+                    tile_data = 'overlay flower2 tile' #pixel square tree
+                if tile == 4:
+                    tile_data = 'dirt tile'
+                Tile(self, tile_data, row_nb, col_nb)
 
     def events(self):
         """Catch all events here."""
@@ -96,10 +94,6 @@ class Game:
                     self.player.move(dy=-1)
                 if event.key == pg.K_DOWN:
                     self.player.move(dy=1)
-                if event.key == pg.K_RETURN:
-                    x, y = self.player.current_position()
-                    self._update_foreground(x, y)
-
 
 # Create game object.
 g = Game()
--- sprites.py
+++ sprites.py
@@ -1,5 +1,7 @@
 import pygame as pg
+
 from settings import *
+from converter import *
 
 class Player(pg.sprite.Sprite):
     """Class that holds everything for the Player Character."""
@@ -7,10 +9,11 @@ class Player(pg.sprite.Sprite):
         self.groups = game.all_sprites
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self.image = pg.image.load("images/overlay player.png").convert_alpha()
+        self.image = pg.image.load("images/128x128 player tile.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
+        self.iso = Converter()
 
     def move(self, dx=0, dy=0):
         """Moves the position of the player in cartesian coordinates."""
@@ -19,17 +22,8 @@ class Player(pg.sprite.Sprite):
 
     def update(self):
         """Updates the isometric position of the player."""
-        self.rect.x, self.rect.y = self._convert()
-
-    def _convert(self):
-        """Converts cartesian coordinates to isometric."""
-        x = self.x
-        y = self.y
-        cart_x = x * TILEWIDTH_HALF
-        cart_y = y * TILEHEIGHT_HALF
-        iso_x = cart_x - cart_y
-        iso_y = (cart_x + cart_y) / 2
-        return iso_x, iso_y
+        print(f'''player cart pos: {self.x}, {self.y}''')
+        self.rect.x, self.rect.y = self.iso.convert_cart(self.x, self.y)
 
     def current_position(self):
         """Current position of the player in isometric coordinates."""
@@ -39,17 +33,47 @@ class Player(pg.sprite.Sprite):
 class Tile(pg.sprite.Sprite):
     """All entities that make up the grid background is currently handled in this class."""
     def __init__(self, game, tile_data, x, y):
-        self.groups = game.all_sprites, game.tiles
+        self.groups = game.tiles #, game.all_sprites
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
         self.image = pg.image.load(f"""images/{tile_data}.png""").convert_alpha()
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
+        self.iso = Converter()
         # Convert cartesian to isometric coordinates
-        cart_x = x * TILEWIDTH_HALF
-        cart_y = y * TILEHEIGHT_HALF
-        iso_x = cart_x - cart_y
-        iso_y = (cart_x + cart_y) / 2
+        iso_x, iso_y = self.iso.convert_cart(self.x, self.y)
         self.rect.x = iso_x
         self.rect.y = iso_y
+
+'''class Grid(pg.sprite.Sprite, Game):
+    """All entities that make up the grid background is currently handled in this class."""
+    def __init__(self, game, map_data):
+        self.groups = game.tiles , game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.iso = Converter()
+        self.map_data = map_data
+
+        for row_nb, row in enumerate(self.map_data):
+            for col_nb, tile in enumerate(row):
+                if tile == 0:
+                    tile_data = '128x128 green tile'
+                if tile == 1:
+                    tile_data = 'overlay seed tile' #pixel square grass
+                if tile == 2:
+                    tile_data = 'overlay flower1 tile' #pixel square flower
+                if tile == 3:
+                    tile_data = 'overlay flower2 tile' #pixel square tree
+                if tile == 4:
+                    tile_data = 'dirt tile'
+                image = pg.image.load(f"""images/{tile_data}.png""").convert_alpha()
+                rect = image.get_rect()
+                x = row_nb
+                y = col_nb
+                # Convert cartesian to isometric coordinates
+                iso_x, iso_y = self.iso.convert_cart(x, y)
+                rect.x = iso_x
+                rect.y = iso_y
+                self.screen.blit(image, rect)'''
+
